1ab213c24927d40e827981b535a558e77f63e80f
Unknown <nbilalis@yahoo.com>
2017-11-29 20:16:17 +0200

    [TASK] Add language definition file.

---
+++ γλώσσα.js
@@ -0,0 +1,62 @@
+﻿/*
+Language: Γλώσσα
+*/
+
+// Language definition guide — highlight.js 9.8.0 documentation - https://goo.gl/jZzggF
+var glossa = function(hljs) {
+	var IDENT_RE_EL = '[a-zA-Zα-ωΑ-Ω][a-zA-Zα-ωΑ-Ω0-9_]*';
+	var KEYWORDS = {
+		keyword:
+			'ΠΡΟΓΡΑΜΜΑ ΑΡΧΗ ΤΕΛΟΣ_ΠΡΟΓΡΑΜΜΑΤΟΣ ' +
+			'ΣΤΑΘΕΡΕΣ ΜΕΤΑΒΛΗΤΕΣ ΑΚΕΡΑΙΕΣ ΠΡΑΓΜΑΤΙΚΕΣ ΧΑΡΑΚΤΗΡΕΣ ΛΟΓΙΚΕΣ ' +
+			'ΔΙΑΒΑΣΕ ΓΡΑΨΕ ' +
+			'ΑΝ ΤΟΤΕ ΑΛΛΙΩΣ_ΑΝ ΑΛΛΙΩΣ ΤΕΛΟΣ_ΑΝ ' +
+			'ΟΣΟ ΕΠΑΝΑΛΑΒΕ ΜΕΧΡΙΣ_ΟΤΟΥ ΓΙΑ ΑΠΟ ΜΕΧΡΙ ΜΕ_ΒΗΜΑ ΜΕ ΒΗΜΑ ΑΡΧΗ_ΕΠΑΝΑΛΗΨΗΣ ΤΕΛΟΣ_ΕΠΑΝΑΛΗΨΗΣ ' +
+			'DIV MOD ' +
+			'ΚΑΙ Ή H ΟΧΙ '
+		,
+		literal:
+			'ΑΛΗΘΗΣ ΨΕΥΔΗΣ',
+		built_in:
+			'ΗΜ ΣΥΝ ΕΦ Α_Τ Τ_Ρ Α_Μ ΛΟΓ Ε'
+	};
+	var COMMENT_MODES = [
+		hljs.COMMENT('!', '$', {relevance: 10})
+	];
+	var STRING = {
+		className: 'string',
+		begin: /'/, end: /'/,
+		contains: [{begin: /''/}]
+	};
+	var SUBST = {
+		className: 'subst',
+		begin: '\\<[^ -=>]', end: '\\>',
+		contains: []
+	};
+	var FUNCTION = {
+		className: 'function',
+		beginKeywords: 'function constructor destructor procedure', end: /[:;]/,
+		keywords: 'function constructor|10 destructor|10 procedure|10',
+		contains: [
+			hljs.TITLE_MODE,
+			{
+				className: 'params',
+				begin: /\(/, end: /\)/,
+				keywords: KEYWORDS,
+				contains: [STRING].concat(COMMENT_MODES)
+			}
+		].concat(COMMENT_MODES)
+	};
+	return {
+		aliases: ['glo', 'glossa'],
+		case_insensitive: false,
+		lexemes: IDENT_RE_EL,
+		keywords: KEYWORDS,
+	//	illegal: /"|\$[G-Zg-z]|\/\*|<\/|\|/,
+		contains: [
+			STRING,
+			SUBST,
+			hljs.NUMBER_MODE
+		].concat(COMMENT_MODES)
+	};
+}
