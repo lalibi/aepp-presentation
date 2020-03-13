a2fbb55d348f9b5435cee3a4b3d50fb0a0bfc222
Nicholaos Bilalis <nikos.bilalis@gmail.com>
2020-03-13 22:06:12 +0200

    Cache busting

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ console.log('WORKER: executing.');
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var sha = '7ee773737661623670fa585316cf97209deaa422';
+var sha = 'THISISTHESHA';
 var version = sha.substr(0, 8) + '::';
 
 /* These resources will be downloaded and cached by the service worker
