2c232b9b8642de1ffa211e1c853bf74e61dec681
GitHub Action <action@github.com>
2020-03-13 20:07:18 +0000

    [DEVOPS] Set CACHE_KEY

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ console.log('WORKER: executing.');
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var sha = 'THISISTHESHA';
+var sha = 'a2fbb55d348f9b5435cee3a4b3d50fb0a0bfc222';
 var version = sha.substr(0, 8) + '::';
 
 /* These resources will be downloaded and cached by the service worker
